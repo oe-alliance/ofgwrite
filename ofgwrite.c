131fb19646c6e428359ace180e809abc7f3b8840
betacentauri <betacentauri@arcor.de>
2014-08-29 12:33:18 +0200

    Kill openvpn to be able to remount rootfs read only

--- ofgwrite.c
+++ ofgwrite.c
@@ -806,6 +806,7 @@ int main(int argc, char *argv[])
 			ret = system("killall CCcam");
 			ret = system("killall hddtemp");
 			ret = system("killall transmission-daemon");
+			ret = system("killall openvpn");
 			// ignore return values, because the processes might not run
 		}
 
