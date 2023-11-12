db2dea286f089d34061bedbef17e9d16381abddf
Captain <captain.onboard@web.de>
2023-11-12 15:31:37 +0100

    update to support new dream rescue 1.11 or newer

--- ofgwrite.c
+++ ofgwrite.c
@@ -470,6 +470,8 @@ int find_image_files(char* p)
 			 || strcmp(entry->d_name, "rootfs.tar.bz2") == 0		// solo4k
 			 || strcmp(entry->d_name, "rootfs.ubi") == 0			// Zgemma H9
 			 || strcmp(entry->d_name, "rootfs.tar.xz") == 0)		// dream
+			 || strcmp(entry->d_name, "rootfs-dreamone.tar.bz2") == 0)		// dreamone
+			 || strcmp(entry->d_name, "rootfs-dreamtwo.tar.bz2") == 0)		// dreamtwo
 			{
 				strcpy(rootfs_filename, path);
 				strcpy(&rootfs_filename[strlen(path)], entry->d_name);
